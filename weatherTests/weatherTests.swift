d1afd1df2ac47e95580911dda9a4935da597e6d0
Matthew Anderson <and3rn3t@icloud.com>
2026-02-04 20:05:07 -0600

    Initial Commit

---
+++ weatherTests/weatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherTests.swift
+//  weatherTests
+//
+//  Created by Matt on 2/4/26.
+//
+
+import Testing
+@testable import weather
+
+struct weatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
